19c179e316942e11fba88f13f3979a290baaad84
Hoai Thai <khanhhoai95@gmail.com>
2022-01-31 22:16:38 +0700

    feat: create messages

---
+++ backend/prisma/migrations/20220131151132_create_messages/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "messages" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "messages_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "messages" ADD CONSTRAINT "messages_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
