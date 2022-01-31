198cbc7fa5aff11e002c49bd00c33ba2b1a96b08
Hoai Thai <khanhhoai95@gmail.com>
2022-01-31 22:17:07 +0700

    feat: install and init socket.io

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,11 +5,14 @@ import fastifyCors from "fastify-cors"
 import fastifyEnv from "fastify-env"
 import fastifyJwt from "fastify-jwt"
 import fastifySwagger from "fastify-swagger"
+import fastifyIO from "fastify-socket.io"
 import { authRoutes } from "./auth"
 import { corsOptions } from "./plugins/cors"
 import { envOptions } from "./plugins/env"
 import { prismaPlugin } from "./plugins/prisma"
 import { swaggerOptions } from "./plugins/swagger"
+import { SocketEvent } from "./messenger/types/SocketEvent"
+import { Message } from "@prisma/client"
 
 const server = fastify({ logger: true })
 
@@ -21,8 +24,25 @@ server.register(fastifyBcrypt)
 server.register(fastifySwagger, swaggerOptions)
 server.register(fastifyAuth)
 server.register(fastifyCors, corsOptions)
+server.register(fastifyIO)
 
 server.register(authRoutes)
+
+server.ready(() => {
+  server.io.on(SocketEvent.Connect, (socket) => {
+    console.log("a user connected")
+    socket.send("connected")
+
+    socket.on(SocketEvent.Disconnect, () => {
+      console.log("a user disconnected")
+    })
+
+    socket.on(SocketEvent.Message, (msg: Message) => {
+      socket.send(msg)
+      console.log("message: " + JSON.stringify(msg))
+    })
+  })
+})
 
 server.listen(8080, (err, address) => {
   if (err) {
---
+++ backend/src/messenger/types/SocketEvent.ts
@@ -0,0 +1,5 @@
+export enum SocketEvent {
+  Connect = "connect",
+  Disconnect = "disconnect",
+  Message = "message",
+}
