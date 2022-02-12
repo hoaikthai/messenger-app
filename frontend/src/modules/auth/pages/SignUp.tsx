bd373dfe6e24921c2fa9af85eba067e69ff4a0d0
Hoai Thai <khanhhoai95@gmail.com>
2022-02-12 21:43:54 +0700

    feat: FE - implement Sign up page

--- frontend/src/modules/auth/pages/SignUp.tsx
+++ frontend/src/modules/auth/pages/SignUp.tsx
@@ -1,7 +1,42 @@
+import { Button, createStyles, Paper, TextInput, Title } from "@mantine/core"
+import { EmptyLayout } from "../layouts/EmptyLayout"
+
 export const SignUp = () => {
+  const { classes } = useStyles()
+
   return (
-    <div>
-      <h1>Sign Up</h1>
-    </div>
+    <EmptyLayout>
+      <Paper className={classes.card}>
+        <Title className={classes.title}>Sign Up</Title>
+        <TextInput placeholder="noobslayer69" label="Username" required size="md" className={classes.input} />
+        <TextInput
+          placeholder="********"
+          label="Password"
+          required
+          size="md"
+          type="password"
+          className={classes.input}
+        />
+        <Button fullWidth size="md" className={classes.action}>
+          Create an account
+        </Button>
+      </Paper>
+    </EmptyLayout>
   )
 }
+
+const useStyles = createStyles((theme) => ({
+  card: {
+    padding: theme.spacing.lg,
+    minWidth: 400,
+  },
+  title: {
+    marginBottom: theme.spacing.xl,
+  },
+  input: {
+    marginBottom: theme.spacing.md,
+  },
+  action: {
+    marginTop: 32,
+  },
+}))
